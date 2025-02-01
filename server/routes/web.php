ace33a2e35bca4c2d49eabb27e1b245d2f1a5847
William <bernarduswilliam.santosa@gmail.com>
2025-02-01 22:22:43 +0700

    admin panel

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TeamController;
-
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,7 +14,15 @@ Route::post('/login', [AuthController::class,'Login'])->name('loginStore');
 Route::get('/logout', [AuthController::class,'ShowLogoutForm'])->name('logout');
 Route::post('/logout', [AuthController::class,'logout'])->name('logoutStore');
 
+
+Route::get('/admin', [AdminController::class, 'index'])->middleware('IsAdminMiddleware')->name('admin.index');
+Route::get('/admin/team/{id}', [AdminController::class, 'show'])->middleware('IsAdminMiddleware')->name('admin.team.show');
+Route::get('/admin/team/{id}/edit', [AdminController::class, 'edit'])->middleware('IsAdminMiddleware')->name('admin.team.edit');
+Route::put('/admin/team/{id}', [AdminController::class, 'update'])->middleware('IsAdminMiddleware')->name('admin.team.update');
+Route::delete('/admin/team/{id}', [AdminController::class, 'destroy'])->middleware('IsAdminMiddleware')->name('admin.team.destroy');
+
+
 Route::middleware('auth.admin')->group(function () {
-    Route::resource('teams', TeamController::class);
+    Route::resource('teams', AdminController::class);
 });
 
